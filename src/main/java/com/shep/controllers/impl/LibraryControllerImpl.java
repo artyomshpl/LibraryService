84907edd1a497b13e6462d4b1ccb14660b421751
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-08-30 18:01:16 +0300

    fix service to depend on abstraction to follow DI from SOLID

--- src/main/java/com/shep/controllers/impl/LibraryControllerImpl.java
+++ src/main/java/com/shep/controllers/impl/LibraryControllerImpl.java
@@ -2,7 +2,7 @@ package com.shep.controllers.impl;
 
 import com.shep.controllers.interfaces.LibraryControllerDocs;
 import com.shep.entities.FreeBook;
-import com.shep.services.LibraryService;
+import com.shep.services.impl.LibraryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,51 +16,51 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LibraryControllerImpl implements LibraryControllerDocs {
 
-    private final LibraryService libraryService;
+    private final LibraryServiceImpl libraryServiceImpl;
 
     @Override
     public Page<FreeBook> getAllFreeBooks(Pageable pageable) {
-        return libraryService.getAllFreeBooks(pageable);
+        return libraryServiceImpl.getAllFreeBooks(pageable);
     }
 
     @Override
     public ResponseEntity<FreeBook> getFreeBookById(@PathVariable Long id) {
-        Optional<FreeBook> freeBook = libraryService.getFreeBookById(id);
+        Optional<FreeBook> freeBook = libraryServiceImpl.getFreeBookById(id);
         return freeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public FreeBook createFreeBook(@RequestBody FreeBook freeBook) {
-        return libraryService.createFreeBook(freeBook);
+        return libraryServiceImpl.createFreeBook(freeBook);
     }
 
     @Override
     public ResponseEntity<FreeBook> updateFreeBook(@PathVariable Long id, @RequestBody FreeBook freeBookDetails) {
-        Optional<FreeBook> updatedFreeBook = libraryService.updateFreeBook(id, freeBookDetails);
+        Optional<FreeBook> updatedFreeBook = libraryServiceImpl.updateFreeBook(id, freeBookDetails);
         return updatedFreeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<Void> deleteFreeBook(@PathVariable Long id) {
-        libraryService.deleteFreeBook(id);
+        libraryServiceImpl.deleteFreeBook(id);
         return ResponseEntity.noContent().build();
     }
 
     @Override
     public ResponseEntity<Void> deleteFreeBookByBookId(@PathVariable Long bookId) {
-        libraryService.deleteFreeBookByBookId(bookId);
+        libraryServiceImpl.deleteFreeBookByBookId(bookId);
         return ResponseEntity.noContent().build();
     }
 
     @Override
     public ResponseEntity<FreeBook> borrowFreeBook(@PathVariable Long bookId) {
-        Optional<FreeBook> borrowedFreeBook = libraryService.borrowFreeBookByBookId(bookId);
+        Optional<FreeBook> borrowedFreeBook = libraryServiceImpl.borrowFreeBookByBookId(bookId);
         return borrowedFreeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<FreeBook> returnFreeBook(@PathVariable Long bookId) {
-        Optional<FreeBook> returnedFreeBook = libraryService.returnFreeBookByBookId(bookId);
+        Optional<FreeBook> returnedFreeBook = libraryServiceImpl.returnFreeBookByBookId(bookId);
         return returnedFreeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/shep/services/impl/LibraryServiceImpl.java
+++ src/main/java/com/shep/services/impl/LibraryServiceImpl.java
@@ -1,8 +1,9 @@
-package com.shep.services;
+package com.shep.services.impl;
 
 
 import com.shep.entities.FreeBook;
 import com.shep.repositories.FreeBookRepository;
+import com.shep.services.interfaces.LibraryServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,22 +15,25 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class LibraryService {
+public class LibraryServiceImpl implements LibraryServiceInterface {
     private final FreeBookRepository freeBookRepository;
 
-
+    @Override
     public Page<FreeBook> getAllFreeBooks(Pageable pageable) {
         return freeBookRepository.findAll(pageable);
     }
 
+    @Override
     public Optional<FreeBook> getFreeBookById(Long id) {
         return freeBookRepository.findById(id);
     }
 
+    @Override
     public FreeBook createFreeBook(FreeBook freeBook) {
         return freeBookRepository.save(freeBook);
     }
 
+    @Override
     public Optional<FreeBook> updateFreeBook(Long id, FreeBook freeBookDetails) {
         return freeBookRepository.findById(id).map(freeBook -> {
             freeBook.setBookId(freeBookDetails.getBookId());
@@ -39,6 +43,7 @@ public class LibraryService {
         });
     }
 
+    @Override
     public Optional<FreeBook> borrowFreeBookByBookId(Long bookId) {
         return freeBookRepository.findByBookId(bookId).map(freeBook -> {
             freeBook.setBorrowedTime(LocalDateTime.now());
@@ -46,6 +51,7 @@ public class LibraryService {
         });
     }
 
+    @Override
     public Optional<FreeBook> returnFreeBookByBookId(Long bookId) {
         return freeBookRepository.findByBookId(bookId).map(freeBook -> {
             freeBook.setReturnTime(LocalDateTime.now());
@@ -53,10 +59,12 @@ public class LibraryService {
         });
     }
 
+    @Override
     public void deleteFreeBook(Long id) {
         freeBookRepository.deleteById(id);
     }
 
+    @Override
     @Transactional
     public void deleteFreeBookByBookId(Long bookId) {
         freeBookRepository.deleteByBookId(bookId);
