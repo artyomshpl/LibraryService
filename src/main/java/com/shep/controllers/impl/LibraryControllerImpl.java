dd46815466154e208cf8ad486ca367365789a14c
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-09-06 03:01:03 +0300

    fix controller to depend on abstraction to follow SOLID

--- src/main/java/com/shep/controllers/impl/LibraryControllerImpl.java
+++ src/main/java/com/shep/controllers/impl/LibraryControllerImpl.java
@@ -2,7 +2,7 @@ package com.shep.controllers.impl;
 
 import com.shep.controllers.interfaces.LibraryControllerDocs;
 import com.shep.dto.FreeBookDTO;
-import com.shep.services.impl.LibraryServiceImpl;
+import com.shep.services.interfaces.LibraryServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,51 +16,51 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LibraryControllerImpl implements LibraryControllerDocs {
 
-    private final LibraryServiceImpl libraryServiceImpl;
+    private final LibraryServiceInterface libraryServiceInterface;
 
     @Override
     public Page<FreeBookDTO> getAllFreeBooks(Pageable pageable) {
-        return libraryServiceImpl.getAllFreeBooks(pageable);
+        return libraryServiceInterface.getAllFreeBooks(pageable);
     }
 
     @Override
     public ResponseEntity<FreeBookDTO> getFreeBookById(@PathVariable Long id) {
-        Optional<FreeBookDTO> freeBook = libraryServiceImpl.getFreeBookById(id);
+        Optional<FreeBookDTO> freeBook = libraryServiceInterface.getFreeBookById(id);
         return freeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public FreeBookDTO createFreeBook(@RequestBody FreeBookDTO freeBookDto) {
-        return libraryServiceImpl.createFreeBook(freeBookDto);
+        return libraryServiceInterface.createFreeBook(freeBookDto);
     }
 
     @Override
     public ResponseEntity<FreeBookDTO> updateFreeBook(@PathVariable Long id, @RequestBody FreeBookDTO freeBookDetails) {
-        Optional<FreeBookDTO> updatedFreeBook = libraryServiceImpl.updateFreeBook(id, freeBookDetails);
+        Optional<FreeBookDTO> updatedFreeBook = libraryServiceInterface.updateFreeBook(id, freeBookDetails);
         return updatedFreeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<Void> deleteFreeBook(@PathVariable Long id) {
-        libraryServiceImpl.deleteFreeBook(id);
+        libraryServiceInterface.deleteFreeBook(id);
         return ResponseEntity.noContent().build();
     }
 
     @Override
     public ResponseEntity<Void> deleteFreeBookByBookId(@PathVariable Long bookId) {
-        libraryServiceImpl.deleteFreeBookByBookId(bookId);
+        libraryServiceInterface.deleteFreeBookByBookId(bookId);
         return ResponseEntity.noContent().build();
     }
 
     @Override
     public ResponseEntity<FreeBookDTO> borrowFreeBook(@PathVariable Long bookId) {
-        Optional<FreeBookDTO> borrowedFreeBook = libraryServiceImpl.borrowFreeBookByBookId(bookId);
+        Optional<FreeBookDTO> borrowedFreeBook = libraryServiceInterface.borrowFreeBookByBookId(bookId);
         return borrowedFreeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<FreeBookDTO> returnFreeBook(@PathVariable Long bookId) {
-        Optional<FreeBookDTO> returnedFreeBook = libraryServiceImpl.returnFreeBookByBookId(bookId);
+        Optional<FreeBookDTO> returnedFreeBook = libraryServiceInterface.returnFreeBookByBookId(bookId);
         return returnedFreeBook.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
